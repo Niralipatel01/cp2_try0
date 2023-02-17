ee50c8204c809384c200e75b538b42cb30a0b036
Niralipatel01 <niralip1810@gmail.com>
2023-02-17 13:20:08 +0000

    Code Synced by DhiWise

---
+++ src/components/index.js
@@ -0,0 +1,34 @@
+import { Datepicker } from "./Datepicker";
+import { FloatingButton } from "./FloatingButton";
+import { Img } from "./Img";
+import { Line } from "./Line";
+import { List } from "./List";
+import { Text } from "./Text";
+import { Button } from "./Button";
+import { SelectBox } from "./SelectBox";
+import { Input } from "./Input";
+import { Radio } from "./Radio";
+import { RadioGroup } from "./RadioGroup";
+import { Switch } from "./Switch";
+import { CheckBox } from "./CheckBox";
+import { Slider } from "./Slider";
+import { SeekBar } from "./SeekBar";
+import { TextArea } from "./TextArea";
+export {
+  Datepicker,
+  FloatingButton,
+  Img,
+  Line,
+  List,
+  Text,
+  Button,
+  SelectBox,
+  Input,
+  Radio,
+  RadioGroup,
+  Switch,
+  CheckBox,
+  Slider,
+  SeekBar,
+  TextArea,
+};
